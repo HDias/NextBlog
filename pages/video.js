3803112e6d4d59df47a10cc8bfd07a1dbe6182bf
Horecio Araújo Dias <horecio@gmail.com>
2021-03-15 00:27:31 -0300

    Link to banner ebook

--- pages/video.js
+++ pages/video.js
@@ -3,10 +3,10 @@ function Video() {
         <div class="container">
             <hr />
             <div class="row">
-                <div class="col-6">
+                <div class="col-md-6 col-xs-12">
                     <iframe src="https://www.youtube.com/embed/TVbApRqzDAA" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen class="w-100 hx-300"></iframe>
                 </div>
-                <div class="col-6">
+                <div class="col-md-6 col-xs-12">
                     <iframe src="https://www.youtube.com/embed/mzMFC_ICloA" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen class="w-100 hx-300"></iframe>
                 </div>
             </div>
