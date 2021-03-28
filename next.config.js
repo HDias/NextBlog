fe8467f0e8f0c9d24cde0d4388285392990a9ac0
Horecio Araújo Dias <horecio@gmail.com>
2021-03-27 23:54:22 -0300

    Change videos List

---
+++ next.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/e-book-10-metodos',
+        destination: 'https://drive.google.com/file/d/0B6TQMP3bQC5xZzRTWjVPN2RoWEk/view',
+        permanent: true,
+      },
+    ]
+  },
+}
--- pages/video.js
+++ pages/video.js
@@ -4,20 +4,40 @@ function Video({ videos }) {
   return (<div>
     <div class="container">
       <hr />
-      <div class="row">
-        {videos.map((video, index) => (
-          <div class="col-md-6 col-xs-12">
-            <iframe src={'https://www.youtube.com/embed/' + video.videoId} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen class="w-100 hx-300"></iframe>
-          </div>
-        ))}
+      <div class="row row-cols-1 row-cols-md-2 g-4">
+        {videos.items.map(({ id, snippet = {} }) => {
+          const { title, description, thumbnails = {} } = snippet;
+          const { medium } = thumbnails;
+          const { videoId } = id;
+          return (
+            <div class="col">
+              <div key={id} class="card shadow">
+                <img src={medium.url} class="card-img-top" alt="..." />
+                <div class="card-body">
+                  <h5 class="card-title">{title}</h5>
+                  <p class="card-text">{description}</p>
+                </div>
+                <div class="card-footer">
+                  <div class="row justify-content-end">
+                    <small class="text-muted col-4">
+                      <a href={`https://www.youtube.com/watch?v=${videoId}`} target="_blank" class="stretched-link card-link"><small>Quero assistir...</small></a>
+                    </small>
+                  </div>
+                </div>
+              </div>
+            </div>
+          )
+        })}
       </div>
+
+      <br />
     </div>
   </div>);
 }
 
-export async function getStaticProps() {
+export async function getServerSideProps() {
   const channelId = 'UCBGmhGIsYxZhEZNqxl_9hKA'
-  const videos = await listYoutube(channelId)
+  const videos = await listYoutube(channelId, 10)
   return {
     props: {
       videos
